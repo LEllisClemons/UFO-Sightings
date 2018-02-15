a2f14bbeb3dacfba005e019ca6cbed54105df48a
Trapeezey <31997938+Trapeezey@users.noreply.github.com>
2018-02-15 12:59:09 -0600

    Add files via upload
    added wordcloud

--- UFO_Sightings.py
+++ UFO_Sightings.py
@@ -15,9 +15,8 @@ from wordcloud import WordCloud, STOPWORDS
 
 with open('scrubbed.csv') as datafile:
     ufo = pd.read_csv(datafile, low_memory=False)
-    
-print(ufo.head(10))
-ufo = ufo.head(10)
+
+ufo = ufo.head(10) #small portion to test
 
 #valid latitudes between 90 and -90
 #valid longitudes between 180, -180
@@ -28,11 +27,10 @@ ufo = ufo[(abs(ufo['latitude']) <= 90.0)]
 ufo = ufo[(abs(ufo['longitude ']) <= 180.0)]
 print(np.dtype(ufo['longitude ']))
 
-print(ufo.head())
 #Add timezones
 my_func = TimezoneFinder().closest_timezone_at
 ufo['timezone'] = ufo.apply(lambda x: my_func(lng=x['longitude '], lat=x['latitude']), axis=1)
-print(ufo.head())
+
 #Determine each time in gmt
 #for x in ufo:
 #   ufo['utc_dt'] = tf.astimezone(utc)
@@ -43,11 +41,20 @@ comment_txt = ufo['comments'].astype(str).sum()
 comment_txt = re.sub('[!@#$&)(/\.;]','', comment_txt)
 comment_txt = re.sub('[0-9]','', comment_txt)
 comment_txt = comment_txt.lower()
-comment_txt[0:2000]
+print(comment_txt[0:2000])
 
-stopwords = set(STOPWORDS)
-wc = WordCloud(max_words=1000, stopwords=stopwords, margin=10,
-               random_state=1).generate(comment_txt)
+#stopwords = set(STOPWORDS)
+#wc = WordCloud(max_words=1000, stopwords=stopwords, margin=10,
+#               random_state=1).generate(comment_txt)
+
+# Generate a word cloud image
+wordcloud = WordCloud().generate(comment_txt)
+
+# Display the generated image:
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
 
 ufo['duration (seconds)'] = ufo['duration (seconds)'].astype(str).map(lambda x: re.sub(r'\W+', '', x))
 ufo['duration (seconds)'] = ufo['duration (seconds)'].astype(float)
@@ -61,4 +68,7 @@ plt.show()
 #worth categorizing into short, medium, long, and extra-long encounters
 #looks like the majority are short encounters
 
+ufo['length'] = 'None'
+
 #where duration < 15 is glimpse, duration 15 < x < 60 is short, 60 < x < 300 is med, 300 < x < 1500 is long, > 1500 is xlong
+#ufo.length[(ufo['duration (seconds)'] < 15)] = 'glimpse'
